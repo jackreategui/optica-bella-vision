c1476d3008d8c5c7f9ec46cff6d068c0f4849688
Jack Reategui Laptop <jackreategui2016@gmail.com>
2023-01-26 12:01:53 -0500

    avance el css con js

--- js/menu.js
+++ js/menu.js
@@ -1,5 +1,13 @@
 const hamburgerIcon = document.querySelector('.nav_hamburger');
+const navOverlay = document.querySelector('.nav_overlay');
 
 hamburgerIcon.addEventListener('click', () => {
   hamburgerIcon.classList.toggle('nav_hamburger--open');
+
+  navOverlay.classList.toggle('nav_overlay--show');
+})
+
+navOverlay.addEventListener('click', (e) => {
+  e.preventDefault();
+  console.log(e.target);
 })
