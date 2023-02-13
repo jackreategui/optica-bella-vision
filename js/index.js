fc2344ec539efc9ace79c528e56bfed89aa2a9c9
Jack Reategui Laptop <jackreategui2016@gmail.com>
2023-02-13 17:42:15 -0500

    añadi unos cambios a los estilos

--- js/index.js
+++ js/index.js
@@ -8,13 +8,31 @@ hamburgerIcon.addEventListener('click', () => {
   navOverlay.classList.toggle('nav_overlay--show');
 })
 
-navOverlay.addEventListener('click', (e) => {
-  e.preventDefault();
-  console.log(e.target);
-})
+// hacer mas grande el textarea cuando sobrepasa su altura
 
 textArea.addEventListener("keyup", e => {
   textArea.style.height = "200px";
   let altura = e.target.scrollHeight;
   textArea.style.height = `${altura}px`;
 })
+
+
+// cerrar menu cuando este abierto
+
+window.addEventListener('scroll', () => {
+  // cierra el menu cuando se hace scroll
+  if (navOverlay.classList.contains('nav_overlay--show')) {
+    closeMenu();
+  };
+})
+
+navOverlay.addEventListener('click', () => {
+  // Cierra el menu cuando se le da click
+  closeMenu();
+})
+
+function closeMenu() {
+  hamburgerIcon.classList.remove('nav_hamburger--open');
+
+  navOverlay.classList.remove('nav_overlay--show');
+};
