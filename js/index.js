954bcba5592d1c2ba4ff9a244366f3b7662918f1
Jack Reategui Laptop <jackreategui2016@gmail.com>
2023-02-13 19:56:48 -0500

    añadi un formulario sin backend

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
 const hamburgerIcon = document.querySelector('.nav_hamburger');
 const navOverlay = document.querySelector('.nav_overlay');
 const textArea = document.querySelector('.input_messege');
+const formulario = document.querySelector('#form');
 
 hamburgerIcon.addEventListener('click', () => {
   hamburgerIcon.classList.toggle('nav_hamburger--open');
@@ -36,3 +37,23 @@ function closeMenu() {
 
   navOverlay.classList.remove('nav_overlay--show');
 };
+
+// formulario
+
+formulario.addEventListener('submit', handleSubmit)
+
+async function handleSubmit(event){
+  event.preventDefault()
+  const form = new FormData(this)
+  const response = await fetch(this.action, {
+    method: this.method,
+    body: form,
+    header: {
+      'Accept': 'application/json'
+    }
+  })
+  if (response.ok) {
+    this.reset()
+    alert('Gracias por contactarme');
+  };
+}
