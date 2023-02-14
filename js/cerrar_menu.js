8a35ace8d67e89db6053f1be6a953aa56b9e5cc9
Jack Reategui Laptop <jackreategui2016@gmail.com>
2023-02-14 10:23:21 -0500

    agregue una nueva pagina de sobre nosotros, ya termine de estilizarla y cree una nueva hoja de js para el cierre del menu

---
+++ js/cerrar_menu.js
@@ -0,0 +1,20 @@
+// cerrar menu cuando este abierto
+
+window.addEventListener('scroll', () => {
+  // cierra el menu cuando se hace scroll
+  if (navOverlay.classList.contains('nav_overlay--show')) {
+    closeMenu();
+  };
+  console.log(hola);
+})
+
+navOverlay.addEventListener('click', () => {
+  // Cierra el menu cuando se le da click
+  closeMenu();
+})
+
+function closeMenu() {
+  hamburgerIcon.classList.remove('nav_hamburger--open');
+
+  navOverlay.classList.remove('nav_overlay--show');
+};
--- js/index.js
+++ js/index.js
@@ -17,27 +17,6 @@ textArea.addEventListener("keyup", e => {
   textArea.style.height = `${altura}px`;
 })
 
-
-// cerrar menu cuando este abierto
-
-window.addEventListener('scroll', () => {
-  // cierra el menu cuando se hace scroll
-  if (navOverlay.classList.contains('nav_overlay--show')) {
-    closeMenu();
-  };
-})
-
-navOverlay.addEventListener('click', () => {
-  // Cierra el menu cuando se le da click
-  closeMenu();
-})
-
-function closeMenu() {
-  hamburgerIcon.classList.remove('nav_hamburger--open');
-
-  navOverlay.classList.remove('nav_overlay--show');
-};
-
 // formulario
 
 formulario.addEventListener('submit', handleSubmit)
