186c190e686b1399a1c4f374997e2e00a3798a91
Jack Reategui Laptop <jackreategui2016@gmail.com>
2023-03-18 11:43:38 -0500

    logre hacer que funcione la calculadora con las primeras medidas y ya añadi los precios, solo falta el resto de medidas :c

--- js/calculadora.js
+++ js/calculadora.js
@@ -14,10 +14,69 @@ const precio = document.querySelector('#precio');
 
 let precioFinal = 0;
 
+const precioResinaCristalBl = 100;
+const precioResinaAr = 160;
+const precioResinaBlue = 220;
+const precioResinaSensitiveFotomateAr = 280;
+const precioResinaSensitiveFotomateBlue = 340;
+const precioResinaFotocromatica = 400;
+const precioResinaTransition = 460;
+const precioResinaTransitionAr = 520;
+const precioResinaTransitionBlue = 580;
+const precioCristalAr = 180;
+const precioCristalPhgPhb = 220;
+const precioCristalPhgPhbAr = 280;
+
 function precioLunas() {
-    if (selectMedidas.selectedIndex == 1 && selectMaterial.selectedIndex == 1) {   
-        precioFinal += 100;
-    }
+    if (selectMedidas.selectedIndex == 1 && selectMaterial.selectedIndex == 1) {
+        precioFinal = 0;
+        precio.textContent = "Precio: s/." + precioFinal;
+        precioFinal += precioResinaCristalBl;
+    } else if (selectMedidas.selectedIndex == 1 && selectMaterial.selectedIndex == 2) {
+        precioFinal = 0;
+        precio.textContent = "Precio: s/." + precioFinal;
+        precioFinal += precioResinaAr;
+    } else if (selectMedidas.selectedIndex == 1 && selectMaterial.selectedIndex == 3) {
+        precioFinal = 0;
+        precio.textContent = "Precio: s/." + precioFinal;
+        precioFinal += precioResinaBlue;
+    } else if (selectMedidas.selectedIndex == 1 && selectMaterial.selectedIndex == 4) {
+        precioFinal = 0;
+        precio.textContent = "Precio: s/." + precioFinal;
+        precioFinal += precioResinaSensitiveFotomateAr;
+    } else if (selectMedidas.selectedIndex == 1 && selectMaterial.selectedIndex == 5) {
+        precioFinal = 0;
+        precio.textContent = "Precio: s/." + precioFinal;
+        precioFinal += precioResinaSensitiveFotomateBlue;
+    } else if (selectMedidas.selectedIndex == 1 && selectMaterial.selectedIndex == 6) {
+        precioFinal = 0;
+        precio.textContent = "Precio: s/." + precioFinal;
+        precioFinal += precioResinaFotocromatica;
+    } else if (selectMedidas.selectedIndex == 1 && selectMaterial.selectedIndex == 7) {
+        precioFinal = 0;
+        precio.textContent = "Precio: s/." + precioFinal;
+        precioFinal += precioResinaTransition;
+    } else if (selectMedidas.selectedIndex == 1 && selectMaterial.selectedIndex == 8) {
+        precioFinal = 0;
+        precio.textContent = "Precio: s/." + precioFinal;
+        precioFinal += precioResinaTransitionAr;
+    } else if (selectMedidas.selectedIndex == 1 && selectMaterial.selectedIndex == 9) {
+        precioFinal = 0;
+        precio.textContent = "Precio: s/." + precioFinal;
+        precioFinal += precioResinaTransitionBlue;
+    } else if (selectMedidas.selectedIndex == 1 && selectMaterial.selectedIndex == 10) {
+        precioFinal = 0;
+        precio.textContent = "Precio: s/." + precioFinal;
+        precioFinal += precioCristalAr;
+    } else if (selectMedidas.selectedIndex == 1 && selectMaterial.selectedIndex == 11) {
+        precioFinal = 0;
+        precio.textContent = "Precio: s/." + precioFinal;
+        precioFinal += precioCristalPhgPhb;
+    } else if (selectMedidas.selectedIndex == 1 && selectMaterial.selectedIndex == 12) {
+        precioFinal = 0;
+        precio.textContent = "Precio: s/." + precioFinal;
+        precioFinal += precioCristalPhgPhbAr;
+    } 
 
     precio.textContent = "Precio: s/." + precioFinal;
 }
